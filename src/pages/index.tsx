c4e49a64118e2155943fe35717d27ca4123a506d
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-04-19 21:11:41 -0300

    config. MyDocument

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import style from '../style/home.module.scss'
+import styles from '../style/home.module.scss'
 
 
 export default function Home() {
   return (
-    <h1 className={style.titile}>Hello World!</h1>
+    <h1 className={styles.titile}>Hello World!</h1>
   )
 }
