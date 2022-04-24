0c16299060ffad678145fa194312a674d2f772f4
Guilherme Cardoso <guilhermecardosowork@gmail.com>
2022-04-24 18:51:08 -0300

    erro

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,50 @@
+import { useSession, signIn } from 'next-auth/react'
+import { useRouter } from 'next/router';
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton ({priceId}:SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const {data: session} = useSession();
+  const router = useRouter();
+
+ async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+    console.log(session);
+    console.log(session?.activeSubscription);
+
+
+    if(session.activeSubscription) {
+      router.push('/posts')
+      return;
+    }
+  
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId })
+      console.log(stripe);
+
+    } catch(err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button 
-      type='button'   
+    <button
+      type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
-      Subscribe Now
+      Subscribe now
     </button>
   )
 }
